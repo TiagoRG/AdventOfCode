c847f7ecf4f7743a569f5911f3e79eb039bafa6b
TiagoRG <35657250+TiagoRG@users.noreply.github.com>
2022-12-03 00:52:21 +0000

    Day2 update

--- days1-5/day2/day2_part1.py
+++ days1-5/day2/day2_part1.py
@@ -25,32 +25,3 @@ with open('inputs/input2.txt', 'r') as f:
             elif rpsMatch[1] == 'Z':
                 score += 3 + 3
     print(f"Total score: {score}")
-
-    # Part 2
-    roundN = 1
-    score = 0
-    for rpsMatch in matches:
-        if rpsMatch[0] == 'A':
-            if rpsMatch[1] == 'X':
-                score += 3
-            elif rpsMatch[1] == 'Y':
-                score += 1 + 3
-            elif rpsMatch[1] == 'Z':
-                score += 2 + 6
-        elif rpsMatch[0] == 'B':
-            if rpsMatch[1] == 'X':
-                score += 1
-            elif rpsMatch[1] == 'Y':
-                score += 2 + 3
-            elif rpsMatch[1] == 'Z':
-                score += 3 + 6
-        elif rpsMatch[0] == 'C':
-            if rpsMatch[1] == 'X':
-                score += 2
-            elif rpsMatch[1] == 'Y':
-                score += 3 + 3
-            elif rpsMatch[1] == 'Z':
-                score += 1 + 6
-    print(f"Total score: {score}")
-
-
